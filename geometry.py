9cca28ee2df457da36f18f8d9af2528299edf5a6
yakovshl <yakovva3@gmail.com>
2026-02-02 11:19:18 +0200

    geometry_

---
+++ geometry.py
@@ -0,0 +1,17 @@
+import math
+
+def rectangle(a,b):
+    return a*b
+
+def triangle(a,b):
+    return a*b/2
+
+def circle_radius(a):
+    return math.pi*(a**2)
+
+def circle_diameter(a):
+    return math.pi*((a/2)**2)
+
+def circle_scope(a):
+    return math.pi*((a/(math.pi*2))**2)
+
