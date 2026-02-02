4168724303860de486da3e3401599d323c9ba660
moshe gertner <mg8540113@gmail.com>
2026-02-02 11:21:34 +0200

    Adding a function to: power

--- function_file.py
+++ function_file.py
@@ -4,9 +4,13 @@ def subtraction(a,b):
     return a - b
 
 
+def add(a,b):
+    return a + b
 
 
+def multiplication(a,b):
+    return a * b
 
+def power(a,b):
+    return a ** b
 
-Multiplication
-Power
